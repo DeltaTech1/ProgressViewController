9815e94159eff8450edb098316e4b293bf3102e1
Fidel <fgonza13@asu.edu>
2016-03-29 18:08:59 -0700

    Fidel Progressview

--- ProgressViewController/ViewController.swift
+++ ProgressViewController/ViewController.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
+    var progressView: UIProgressView?
+    var progressLabel: UILabel?
+    var timer: NSTimer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        addControls()
+        addGestures()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func addControls() {
+        progressView = UIProgressView(progressViewStyle: UIProgressViewStyle.Default)
+        progressView?.center = self.view.center
+        
+        view.addSubview(progressView!)
+        
+        progressLabel = UILabel()
+        let frame = CGRectMake(view.center.x - 25, view.center.y - 100, 100, 50)
+        progressLabel?.frame = frame
+        
+        view.addSubview(progressLabel!)
+    }
+    
+    func addGestures() {
+        let tap = UITapGestureRecognizer(target: self, action: "handleTap:")
+        tap.numberOfTapsRequired = 1
+        
+        let doubleTap = UITapGestureRecognizer(target: self, action: "handleDoubleTap:")
+        doubleTap.numberOfTapsRequired = 2
+        
+        view.addGestureRecognizer(tap)
+        view.addGestureRecognizer(doubleTap)
+        tap.requireGestureRecognizerToFail(doubleTap)
+    }
+    
+    func handleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .Ended {
+            timer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector:"updateProgress", userInfo: nil, repeats: true)
+        }
+    }
+    func handleDoubleTap(sender: UITapGestureRecognizer) {
+        if sender.state == .Ended {
+            progressView?.progress = 0.0
+            progressLabel?.text = "0 %"
+            timer?.invalidate()
+        }
     }
 
-
+    func updateProgress() {
+        progressView?.progress += 0.05
+        
+        let progressValue = self.progressView?.progress
+        progressLabel?.text = "\(progressValue! * 100) %"
+    }
 }
 
